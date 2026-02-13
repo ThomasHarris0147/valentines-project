3ee3616519a0cee9cec4a27e6f0e508deea96c03
thegang_zThomasH <zthomash@pttep.com>
2026-02-13 19:23:21 +0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/valentines/' : '/',
+  base: process.env.NODE_ENV === 'production' 
+    ? (process.env.GITHUB_REPOSITORY 
+        ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+        : './')
+    : '/',
 })
 
