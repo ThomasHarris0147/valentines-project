c8823e121f1b7ef524e3ce397e01199aa48f5638
thegang_zThomasH <zthomash@pttep.com>
2026-02-13 19:23:50 +0700

    Fix base path to match repository name

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' 
-    ? (process.env.GITHUB_REPOSITORY 
-        ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
-        : './')
-    : '/',
+  base: process.env.NODE_ENV === 'production' ? '/valentines-project/' : '/',
 })
 
